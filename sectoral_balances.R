1f4b10693fbb78f7e7cb926fb22fbb21fbdc8166
RomainDsgz <romaindsgz@users.noreply.github.com>
2024-02-13 22:59:59 +0100

    Four sectors
    In this update, I corrected some typos, made the code clearer and added lines to create a graph on sectoral balances with four sectors

--- sectoral_balances.R
+++ sectoral_balances.R
@@ -20,10 +20,11 @@ all_files <- lapply(files, function(file) {
 
 ameco <- do.call(rbind, all_files)
 ameco <- tibble::as_tibble(ameco)
-ameco <- ameco[, -ncol(ameco)] #Drop stray/empty last column
+#Drop stray/empty last column
+ameco <- ameco[, -ncol(ameco)] 
 names(ameco) <- tolower(names(ameco))
 # Extract short country names 
-ameco$cntry <- regmatches(ameco$code, regexpr("^[[:alnum:]+", ameco$code))
+ameco$cntry <- regmatches(ameco$code, regexpr("^[[:alnum:]+]", ameco$code))
 
 # Convert to long format 
 ameco <- gather(ameco, key = year, value = value, starts_with("x"))
@@ -31,7 +32,7 @@ ameco$year <- gsub("x", "", ameco$year)
 ameco$year <- as.numeric(ameco$year)
 ameco$value <- suppressWarnings(as.numeric(ameco$value))
 
-# Save preparer data                                
+# Save prepared data                                
 save(ameco, file = "C:/Users/rdesg/Documents/sectoral_balances/ameco.RData", 
      compress = "xz")
 
@@ -47,19 +48,20 @@ ch <- subset(ameco, country == "Switzerland")
 
 # Codes of the different variables for Net lending - Net borrowing (Mrd CHF)
 # Find codes for Net lending/Net borrowing 
-# UBLG = General Government (Mrd CHF )
-# UBLA = Rest of the World : UBLA (Mrd CHF)
-# UBLC = Corporations (Financial and Non-Financial) (Mrd CHF)
-# UBLH = Households and non-profit Institutions serving households (Mrd CHF)
-# UVGD = Gross Domestic Product (Current prices) 
+# UBLG = General Government (Mrd National Currency)
+# UBLA = Rest of the World : UBLA (Mrd National Currency)
+# UBLC = Corporations (Financial and Non-Financial) (Mrd National Currency)
+# UBLH = Households and non-profit Institutions serving households (Mrd National Currency)
+# UVGD = Gross Domestic Product (Current prices)
+
 # Public Sector 
 UBLG <- subset(ch, code=="CHE.1.0.0.0.UBLG")
 # Rest of the world 
 UBLA <- subset(ch, code=="CHE.1.0.0.0.UBLA")
 # Private sector 
-  # Corporations
+# Corporations
 UBLC <- subset(ch, code=="CHE.1.0.0.0.UBLC")
-  # Households and non-profit Institutions serving households 
+# Households and non-profit Institutions serving households 
 UBLH <- subset(ch, code=="CHE.1.0.0.0.UBLH")
 
 # GDP 
@@ -90,20 +92,56 @@ dfw$share_corp <- (dfw$UBLC / dfw$UVGD)*100
 dfw$share_household <- (dfw$UBLH / dfw$UVGD)*100
 dfw$share_priv <- ((dfw$UBLC + dfw$UBLH) / dfw$UVGD)*100
 
+dfw$test <- dfw$share_public + dfw$share_priv + dfw$share_row
+dfw$test
+### Sectoral balances with three sectors 
+
+# Data frame for 3 sectors (Public, private, rest of the world)
+melted_df3 <- dfw %>% 
+  select(year, share_public, share_priv, share_row) %>% 
+  gather(key = "variable", value = "value", -year)
 melted_df <- dfw%>%
   select(year, share_public, share_corp, share_household, share_row) %>%
   gather(key = "variable", value = "value", -year)
 
-# Graph  
-my_plot <- ggplot() + 
-  geom_bar(data = subset(melted_df, year >= 1995), aes(x = year, y = value, fill = variable), stat = "identity") + 
-  scale_fill_manual(values = c("share_public" = "red", "share_row" = "blue", "share_corp" = "green", "share_household" = "yellow"),
-                    labels = c("Public sector", "Rest of the World", "Corporations", "Household")) +
-  labs(x = "Year", y = "Net lending - Net borrowing", fill = "Balance", color = "") + 
+
+# Graph  - 3 Sectors 
+# Colors 
+colors3 <- c("indianred3", "chartreuse4", "turquoise3")
+plot_3sectors <- ggplot() + 
+  geom_bar(data = subset(melted_df3, year >= 1995), aes(x = year, y = value, fill = variable), stat = "identity") + 
+  scale_fill_manual(values = colors3,
+                    labels = c("Public sector", "Rest of the World", "Private Sector")) +
+  labs(x = "Year", y = "Net lending - Net borrowing", fill = "Balance") + 
   theme_minimal() + 
   theme(plot.background = element_rect(fill = "white")) + 
-  scale_x_continuous(breaks = seq(1995, 2023, by = 5)) + 
+  scale_x_continuous(breaks = seq(1995, 2023, by = 5)) +
+  #scale_y_continuous(labels = scales::percent_format()) +
+  scale_fill_manual(values = colors3) +
   labs(title = "Sectoral balances",
-       subtitle = "Switzerland, 19995-2023", 
+       subtitle = "Switzerland, 1995-2023", 
        caption = "Source: AMECO")
-print(my_plot)  
+print(plot_3sectors)  
+
+### Sectoral balances with four sectors 
+melted_df4 <- dfw %>% 
+  select(year, share_public, share_corp, share_household, share_row) %>% 
+  gather(key = "variable", value = "value", -year)
+# Graph  - 3 Sectors 
+# Colors 
+colors4 <- c("indianred3", "chartreuse4", "turquoise3", "plum4")
+plot_4sectors <- ggplot() + 
+  geom_bar(data = subset(melted_df4, year >= 1995), aes(x = year, y = value, fill = variable), stat = "identity") + 
+  scale_fill_manual(values = colors4,
+                    labels = c("Public sector", "Corporations", "Households", "Rest of the World")) +
+  labs(x = "Year", y = "Net lending - Net borrowing", fill = "Balance") + 
+  theme_minimal() + 
+  theme(plot.background = element_rect(fill = "white")) + 
+  scale_x_continuous(breaks = seq(1995, 2023, by = 5)) +
+  #scale_y_continuous(labels = scales::percent_format()) +
+  scale_fill_manual(values = colors4) +
+  labs(title = "Sectoral balances",
+       subtitle = "Switzerland, 1995-2023", 
+       caption = "Source: AMECO")
+print(plot_4sectors)  
+
